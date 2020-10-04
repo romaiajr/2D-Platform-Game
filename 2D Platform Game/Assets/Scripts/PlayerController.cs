ffa22bb41946a779792c9f920087a9cf38fbe7d8
Roberto Maia <romaia.uefs@gmail.com>
2020-10-04 14:18:07 -0300

    Player Jump and Move

---
+++ 2D Platform Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    private Rigidbody2D rig;
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move(){
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+    }
+
+    void Jump(){
+        if(Input.GetButtonDown("Jump")){
+            rig.AddForce(new Vector2(0f,JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
