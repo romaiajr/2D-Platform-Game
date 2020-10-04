9d6c389fb70da233a635372fa0f9c20ba44c47a0
Roberto Maia <romaia.uefs@gmail.com>
2020-10-04 15:00:39 -0300

    Double Jumb e Player Grudando

--- 2D Platform Game/Assets/Scripts/PlayerController.cs
+++ 2D Platform Game/Assets/Scripts/PlayerController.cs
@@ -3,30 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    public float Speed;
-    public float JumpForce;
-    private Rigidbody2D rig;
-    void Start()
-    {
+{public float Speed;
+public float JumpForce;
+
+public bool isJumping;
+public bool doubleJump;
+
+private Rigidbody2D rig;
+
+
+    // Start is called before the first frame update
+    void Start(){
         rig = GetComponent<Rigidbody2D>();
     }
 
-    
-    void Update()
-    {
+    // Update is called once per frame
+    void Update(){
         Move();
         Jump();
     }
 
     void Move(){
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * Speed;
+        Vector3 moviment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += moviment * Time.deltaTime * Speed;
     }
 
     void Jump(){
         if(Input.GetButtonDown("Jump")){
-            rig.AddForce(new Vector2(0f,JumpForce), ForceMode2D.Impulse);
+            if(!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce/1.5f), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+            
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision){
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = true;
         }
     }
 }
